9682df47344f5c7fcc03beaf8604b3587c59cffa
daorejuela1 <daorejuela1@outlook.com>
2022-06-30 21:58:26 -0500

    Added second task

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Calculates the shape of a matrix
+"""
+
+
+def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix
+    """
+    if type(matrix) is list:
+        return [len(matrix)] + matrix_shape(matrix[0])
+    else:
+        return [1]
