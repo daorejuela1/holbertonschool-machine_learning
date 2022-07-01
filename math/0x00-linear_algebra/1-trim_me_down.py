2b8912f5130b619217cbc1189f2d95f207640735
daorejuela1 <daorejuela1@outlook.com>
2022-06-30 21:51:46 -0500

    Added trim me down exercise

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-"""
-This program will trim the array down to the first two numbers, the last five numbers, and the 2nd through 6th numbers.
-"""
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
-the_middle.append(matrix[1][2:4]) for i
+[the_middle.append(matrix[i][2:4]) for i in range(len(matrix))]
 print("The middle columns of the matrix are: {}".format(the_middle))
